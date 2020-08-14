91df27040bbfea103558cbc8c1fc9696f4b3c1ed
Jonathan Cummings <jonathan.cummings@gmail.com>
2020-08-14 17:46:47 -0400

    Shiny app to store and load form inputs in a dropbox folder

---
+++ ShinyFormDropbox/app.R
@@ -0,0 +1,90 @@
+# Store form imput on dropbox with this app
+
+# Recreation of a form submission shiny app found here:
+# https://deanattali.com/blog/shiny-persistent-data-storage/
+
+
+# Load libraries
+library(tidyverse)
+library(shiny)
+library(DT)
+library(digest)
+library(rdrop2)
+
+# add a directory to store the responses
+outputDir <- "responses"
+
+# Function to save entries to a .csv
+saveData <- function(data) {
+    # transpose the data (why?)
+    data <- t(data)
+    # Create a unique file name
+    fileName <- sprintf("%s_%s.csv", as.integer(Sys.time()), digest::digest(data))
+    # Write the data to a temporary file locally
+    filePath <- file.path(tempdir(), fileName)
+    write.csv(data, filePath, row.names = FALSE, quote = TRUE)
+    # Upload the file to Dropbox
+    drop_upload(filePath, path = outputDir, dtoken = token)
+}
+
+
+loadData <- function() {
+    # Read all the files into a list
+    filesInfo <- drop_dir(outputDir,dtoken = token)
+    filePaths <- filesInfo$path_display
+    data <- lapply(filePaths, drop_read_csv, stringsAsFactors = FALSE) %>% 
+        do.call(rbind, .)
+    data
+}
+
+# Define the fields we want to save from the form
+fields <- c("name", "used_shiny", "r_num_years")
+
+# Define UI
+ui <- fluidPage(
+    
+    # Application title
+    titlePanel("Mimicking a Google Form with a Shiny app"),
+    
+    # Sidebar
+    sidebarLayout(
+        # Create three inputs
+        sidebarPanel(
+            textInput("name", "Name", ""),
+            checkboxInput("used_shiny", "I've built a Shiny app in R before", FALSE),
+            sliderInput("r_num_years", "Number of years using R",
+                        0, 25, 2, ticks = FALSE),
+            actionButton("submit", "Submit")
+        ), # end sidebarPanel
+        
+        # Show a table of responses
+        mainPanel(
+            DT::dataTableOutput("responses", width = 300), tags$hr()
+        ) # end mainPanel
+    ) # end sdiebarLayout
+) # end fluidpage
+
+# Define server logic required to draw a histogram
+server <- function(input, output, session) {
+    
+    # Whenever a field is filled, aggregate all form data
+    formData <- reactive({
+        data <- sapply(fields, function(x) input[[x]])
+        data
+    })
+    
+    # When the Submit button is clicked, save the form data
+    observeEvent(input$submit, {
+        saveData(formData())
+    })
+    
+    # Show the previous responses
+    # (update with current response when Submit is clicked)
+    output$responses <- DT::renderDataTable({
+        input$submit
+        loadData()
+    })
+} # send server
+
+# Run the application 
+shinyApp(ui = ui, server = server)
